3dd4e22c728bd110c033768438931ea201c622c7
Mark W. Jarrell <mark.jarrell@gmail.com>
2008-12-23 18:11:41 +0000

    Feature addition. Allows resizing of line-height to match changes in text size.

--- text_resize.js
+++ text_resize.js
@@ -3,58 +3,76 @@
 if (Drupal.jsEnabled) {
   $(document).ready(function(){
     // Config variables
-    var largestSizeAllowed = text_resize_maximum; // Based on admin form variable
-    var smallestSizeAllowed = text_resize_minimum; // Based on admin form variable
+    var largest_size_allowed = text_resize_maximum; // Based on admin form variable
+    var smallest_size_allowed = text_resize_minimum; // Based on admin form variable
+		var line_height_allow = text_resize_line_height_allow; // Based on admin form variable
     // Which div or page element are we resizing?
     if ($('DIV.left-corner').length > 0) {
-      var elementToResize = $('DIV.left-corner'); // Main body div for Garland
+      var element_to_resize = $('DIV.left-corner'); // Main body div for Garland
     } else if ($('#content-inner').length > 0) {
-      var elementToResize = $('#content-inner'); // Main body div for Zen-based themes
+      var element_to_resize = $('#content-inner'); // Main body div for Zen-based themes
+    } else if ($('#squeeze > #content').length > 0) {
+      var element_to_resize = $('#squeeze > #content'); // Main body div for Zen Classic
     } else if ($('#'+text_resize_scope).length > 0) {
-      var elementToResize = $('#'+text_resize_scope); // ID specified by admin
+      var element_to_resize = $('#'+text_resize_scope); // ID specified by admin
+    } else if ($('.'+text_resize_scope).length > 0) {
+      var element_to_resize = $('.'+text_resize_scope); // CLASS specified by admin
     } else {
-      var elementToResize = $(text_resize_scope); // It's just a tag specified by admin
+      var element_to_resize = $(text_resize_scope); // It's just a tag specified by admin
     }
-
     // Set the initial font size if necessary
     if ($.cookie('text_resize') != null) {
-      elementToResize.css('font-size', parseFloat($.cookie('text_resize')));
+      element_to_resize.css('font-size', parseFloat($.cookie('text_resize')) + 'px');
       //alert( "Should be size: " + $.cookie('text_resize'));
     } else {
       //alert('Couldn\'t find text_resize cookie.');
     }
+    if (line_height_allow) {
+      //alert('line height adjustment allowed! The current line-height is '+parseFloat(element_to_resize.css('line-height'), 10));
+			// Set the initial line height if necessary
+      if ($.cookie('text_resize_line_height') != null) {
+        element_to_resize.css('line-height', parseFloat($.cookie('text_resize_line_height')) + 'px');
+      }
+		}
     // Changer links will change the text size when clicked
     $('a.changer').click(function(){
-      // set the current font size of .mainText as a var called currentSize
-      var currentFontSize = elementToResize.css('font-size');
-      // parse the number value out of the font size value, set as a var called 'num'
-      var currentFontSizeNum = parseFloat(currentFontSize, 10);
+      // Set the current font size of the specified section as a variable
+      var currentFontSize = parseFloat(element_to_resize.css('font-size'), 10);
+			//alert('currentFontSize = '+currentFontSize);
+			// Set the current line-height
+			var current_line_height = parseFloat(element_to_resize.css('line-height'), 10);
+			//alert('current_line_height = '+current_line_height);
       // javascript lets us choose which link was clicked, by ID
       if (this.id == 'text_resize_fontBig'){
-        var newFontSize = currentFontSizeNum * 1.2;
-        // Allow resizing as long as font size doesn't go above largestSizeAllowed.
-        if (newFontSize <= largestSizeAllowed) {
-          $.cookie('text_resize', newFontSize, { path: '/' });
-          var allowChange = true;
+        var new_font_size = currentFontSize * 1.2;
+        if (line_height_allow) { var new_line_height = current_line_height * 1.2; }
+        // Allow resizing as long as font size doesn't go above largest_size_allowed.
+        if (new_font_size <= largest_size_allowed) {
+          $.cookie('text_resize', new_font_size, { path: '/' });
+          if (line_height_allow) { $.cookie('text_resize_line_height', new_line_height, { path: '/' }); }
+          var allow_change = true;
         }
       } else if (this.id == 'text_resize_fontSmall'){
-        var newFontSize = currentFontSizeNum * 0.8;
-        if (newFontSize >= smallestSizeAllowed) {
-          // Allow resizing as long as font size doesn't go below smallestSizeAllowed.
-          $.cookie('text_resize', newFontSize, { path: '/' });
-          var allowChange = true;
+        var new_font_size = currentFontSize * 0.8;
+        if (line_height_allow) { var new_line_height = current_line_height * 0.8; }
+        if (new_font_size >= smallest_size_allowed) {
+          // Allow resizing as long as font size doesn't go below smallest_size_allowed.
+          $.cookie('text_resize', new_font_size, { path: '/' });
+          if (line_height_allow) { $.cookie('text_resize_line_height', new_line_height, { path: '/' }); }
+          var allow_change = true;
         } else {
-          // If it goes below smallestSizeAllowed, just leave it at smallestSizeAllowed.
-          $.cookie('text_resize', smallestSizeAllowed, { path: '/' });
-          var resetSize = true;
+          // If it goes below smallest_size_allowed, just leave it at smallest_size_allowed.
+          $.cookie('text_resize', smallest_size_allowed, { path: '/' });
+          var reset_size = true;
         }
       }
       // jQuery lets us set the font Size value of the main text div
-      if (allowChange == true) {
-        elementToResize.css('font-size', newFontSize);
+      if (allow_change == true) {
+        element_to_resize.css('font-size', new_font_size + 'px'); // Add 'px' onto the end, otherwise ems are used as units by default
+				if (line_height_allow) { element_to_resize.css('line-height', new_line_height + 'px'); }
         return false;
-      } else if (resetSize == true) {
-        elementToResize.css('font-size', smallestSizeAllowed);
+      } else if (reset_size == true) {
+        element_to_resize.css('font-size', smallest_size_allowed);
         return false;
       }
     });

f817c52ee0fe1f672ae906a6cdafa647b25c20df
Mark W. Jarrell <mark.jarrell@gmail.com>
2010-12-29 23:14:13 +0000

    #357092 Change to reset javascript.

--- text_resize.js
+++ text_resize.js
@@ -13,12 +13,9 @@
         else {
           var element_to_resize = $(text_resize_scope); // It's just a tag specified by admin
         }
-  		}
-  		else { // Look for some default scopes that might exist.
-        if ($('#main').length > 0) {
-          var element_to_resize = $('#main'); // Main body div for Bartik
-        }
-        else if ($('DIV.left-corner').length > 0) {
+      }
+      else { // Look for some default scopes that might exist.
+        if ($('DIV.left-corner').length > 0) {
           var element_to_resize = $('DIV.left-corner'); // Main body div for Garland
         }
         else if ($('#content-inner').length > 0) {
@@ -27,30 +24,23 @@
         else if ($('#squeeze > #content').length > 0) {
           var element_to_resize = $('#squeeze > #content'); // Main body div for Zen Classic
         }
-  		}
+      }
       // Set the initial font size if necessary
       if ($.cookie('text_resize') != null) {
         element_to_resize.css('font-size', parseFloat($.cookie('text_resize')) + 'px');
-        //alert( "Should be size: " + $.cookie('text_resize'));
-      }
-      else {
-        //alert('Couldn\'t find text_resize cookie.');
       }
       if (text_resize_line_height_allow) {
-        //alert('line height adjustment allowed! The current line-height is '+parseFloat(element_to_resize.css('line-height'), 10));
-  			// Set the initial line height if necessary
+        // Set the initial line height if necessary
         if ($.cookie('text_resize_line_height') != null) {
           element_to_resize.css('line-height', parseFloat($.cookie('text_resize_line_height')) + 'px');
         }
-  		}
+      }
       // Changer links will change the text size when clicked
       $('a.changer').click(function() {
         // Set the current font size of the specified section as a variable
         var currentFontSize = parseFloat(element_to_resize.css('font-size'), 10);
-  			//alert('currentFontSize = '+currentFontSize);
-  			// Set the current line-height
-  			var current_line_height = parseFloat(element_to_resize.css('line-height'), 10);
-  			//alert('current_line_height = '+current_line_height);
+        // Set the current line-height
+        var current_line_height = parseFloat(element_to_resize.css('line-height'), 10);
         // javascript lets us choose which link was clicked, by ID
         if (this.id == 'text_resize_increase') {
           var new_font_size = currentFontSize * 1.2;
@@ -62,10 +52,10 @@
             var allow_change = true;
           }
           else {
-  					$.cookie('text_resize', text_resize_maximum, { path: '/' });
+            $.cookie('text_resize', text_resize_maximum, { path: '/' });
             if (text_resize_line_height_allow) { $.cookie('text_resize_line_height', text_resize_line_height_max, { path: '/' }); }
             var reset_size_max = true;
-  				}
+          }
         }
         else if (this.id == 'text_resize_decrease') {
           var new_font_size = currentFontSize / 1.2;
@@ -79,29 +69,34 @@
           else {
             // If it goes below text_resize_minimum, just leave it at text_resize_minimum.
             $.cookie('text_resize', text_resize_minimum, { path: '/' });
-  					if (text_resize_line_height_allow) { $.cookie('text_resize_line_height', text_resize_line_height_min, { path: '/' }); }
+            if (text_resize_line_height_allow) { $.cookie('text_resize_line_height', text_resize_line_height_min, { path: '/' }); }
             var reset_size_min = true;
           }
         }
         else if (this.id == 'text_resize_reset') {
-          $.cookie('text_resize', text_resize_minimum, { path: '/' });
-  				if (text_resize_line_height_allow) { $.cookie('text_resize_line_height', text_resize_line_height_min, { path: '/' }); }
-          var reset_size_min = true;
+          $.cookie('text_resize', null, { path: '/' });
+          if (text_resize_line_height_allow) { $.cookie('text_resize_line_height', null, { path: '/' }); }
+          var reset_size_original = true;
         }
         // jQuery lets us set the font size value of the main text div
         if (allow_change == true) {
           element_to_resize.css('font-size', new_font_size + 'px'); // Add 'px' onto the end, otherwise ems are used as units by default
-  				if (text_resize_line_height_allow) { element_to_resize.css('line-height', new_line_height + 'px'); }
+          if (text_resize_line_height_allow) { element_to_resize.css('line-height', new_line_height + 'px'); }
           return false;
         }
         else if (reset_size_min == true) {
           element_to_resize.css('font-size', text_resize_minimum + 'px');
-  				if (text_resize_line_height_allow) { element_to_resize.css('line-height', text_resize_line_height_min + 'px'); }
+          if (text_resize_line_height_allow) { element_to_resize.css('line-height', text_resize_line_height_min + 'px'); }
           return false;
         }
         else if (reset_size_max == true) {
           element_to_resize.css('font-size', text_resize_maximum + 'px');
-  				if (text_resize_line_height_allow) { element_to_resize.css('line-height', text_resize_line_height_max + 'px'); }
+          if (text_resize_line_height_allow) { element_to_resize.css('line-height', text_resize_line_height_max + 'px'); }
+          return false;
+        }
+        else if (reset_size_original == true) {
+          element_to_resize.css('font-size', null);
+          if (text_resize_line_height_allow) { element_to_resize.css('line-height', null); }
           return false;
         }
       });
